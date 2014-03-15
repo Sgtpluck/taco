a077bb078284ecd1c5422efe64f75077fae09897
Davida Marion <davidamarion@gmail.com>
2014-03-15 16:27:08 -0700

    stub test attempt not working, must revisit soon

--- spec/models/food_finder_spec.rb
+++ spec/models/food_finder_spec.rb
@@ -14,14 +14,18 @@ describe FoodFinder do
 
     describe 'request' do
       it 'should create a geopoint request object' do
-        expect(tacos.food_and_location_request).to be_an_instance_of(Yelp::V1::Review::Request::GeoPoint)
+        expect(tacos.food_and_location_request).to be_an_instance_of(Yelp::V2::Search::Request::GeoPoint)
       end
     end
 
     describe 'sorted_response' do
-      let(:response) { { "distance"=>271.51581067773924, "mobile_url"=>"http://m.yelp.com/biz/tacos-chukis-seattle", "rating_img_url"=>"http://s3-media2.ak.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png", "review_count"=>260, "name"=>"Tacos Chukis", "location"=>{"display_address"=>["219 Broadway E", "(b/t John St & Olive Way)", "Capitol Hill", "Seattle, WA 98102"], "neighborhoods"=>["Capitol Hill"], "postal_code"=>"98102", "country_code"=>"US", "address"=>["219 Broadway E"], "state_code"=>"WA"}, "phone"=>"2063284447", "image_url"=>"http://s3-media3.ak.yelpcdn.com/bphoto/aMcEM2eXL2esBRRCflSctQ/ms.jpg", "categories"=>[["Mexican", "mexican"]], "is_closed"=>false, "rating_img_url_small"=>"http://s3-media2.ak.yelpcdn.com/assets/2/www/img/a5221e66bc70/ico/stars/v1/stars_small_4_half.png" } }
+      it 'should do something' do
+        # response = VCR.use_cassette 'taco' do
+        #    tacos.yelp_response
+        #  end
+        #  expect(response).to be_an_instance_of Array
+      end
     end
-
 
 
   end
