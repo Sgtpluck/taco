a9a5ff0402107976027bed19cf8f460279f559f6
Davida Marion <davidamarion@gmail.com>
2014-03-16 13:39:12 -0700

    changes config to maybe be better for heroku

--- config/initializers/client.rb
+++ config/initializers/client.rb
@@ -1,6 +1,12 @@
 Yelp.configure(
-  yws_id:          Figaro.env.yelp_ywsid,
-  consumer_key:    Figaro.env.yelp_consumer_key,
-  consumer_secret: Figaro.env.yelp_consumer_secret,
-  token:           Figaro.env.yelp_token,
-  token_secret:    Figaro.env.yelp_token_secret)
+  # yws_id:          Figaro.env.yelp_ywsid,
+  # consumer_key:    Figaro.env.yelp_consumer_key,
+  # consumer_secret: Figaro.env.yelp_consumer_secret,
+  # token:           Figaro.env.yelp_token,
+  # token_secret:    Figaro.env.yelp_token_secret)
+  yws_id:           ENV["YELP_YWSID"],
+  consumer_key:     ENV["YELP_CONSUMER_KEY"],
+  consumer_secret:  ENV["YELP_CONSUMER_SECRET"],
+  token:            ENV["YELP_TOKEN"],
+  token_secret:     ENV["YELP_TOKEN_SECRET"]
+  )
