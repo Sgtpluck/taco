20f7acd082f187614ca8bcbe99ab4225dbb26855
Davida Marion <davidamarion@gmail.com>
2014-03-28 10:31:31 -0700

    removing yelp code

--- app/models/food_finder.rb
+++ app/models/food_finder.rb
@@ -1,31 +1,11 @@
 class FoodFinder
   include Yelp::V2::Search::Request
-  attr_accessor :location_and_type_hash, :longitude, :latitude, :term
+  attr_accessor :longitude, :latitude, :term
 
   def initialize(location_and_type_hash)
-    @location_and_type_hash = location_and_type_hash
     @longitude = location_and_type_hash[:longitude]
     @latitude = location_and_type_hash[:latitude]
     @term = location_and_type_hash[:path]
-  end
-
-  def client
-    Yelp::Base.client
-  end
-
-  def food_and_location_request
-    GeoPoint.new(:term => @location_and_type_hash[:path],
-                 :latitude => @location_and_type_hash[:latitude],
-                 :longitude => @location_and_type_hash[:longitude],
-                 :radius => 2)
-  end
-
-  def sorted_yelp_response
-    client.search(food_and_location_request)['businesses'].sort_by { |business, distance| business['distance'] }
-  end
-
-  def closest
-    sorted_yelp_response.first 
   end
 
   def sorted_google_response
--- spec/models/food_finder_spec.rb
+++ spec/models/food_finder_spec.rb
@@ -5,33 +5,11 @@ describe FoodFinder do
   describe 'methods' do
     let(:tacos) {FoodFinder.new({ latitude: 47.622,
                                   longitude: -122.313,
-                                  food: 'tacos' }) }
+                                  term: 'tacos' }) }
     let(:bad_request) {FoodFinder.new({  longitude: -122.313,
-                                  food: 'tacos' }) }
+                                  term: 'tacos' }) }
   
-    describe 'client' do
-      it 'should create a Yelp Client object' do
-        expect(tacos.client).to be_an_instance_of(Yelp::Client)
-      end
-    end
 
-    describe 'request' do
-      it 'should create a geopoint request object' do
-        expect(tacos.food_and_location_request).to be_an_instance_of(Yelp::V2::Search::Request::GeoPoint)
-      end
-    end
-
-    describe 'sorted_yelp_response' do
-
-      it 'should return an array and sort it by distance' do
-        response = VCR.use_cassette 'taco' do
-           tacos.sorted_yelp_response
-         end
-
-         expect(response).to be_an_instance_of Array
-         expect(response.first['distance']).to be < response.last['distance']
-      end
-    end
 
     describe 'sorted_google_response' do
       it 'should return an array' do
@@ -53,27 +31,6 @@ describe FoodFinder do
         end
 
         expect(closest_taco). to eq response.first
-      end
-    end
-
-    describe 'closest' do
-      it 'should return the first element of the sorted array if there is at least one response from yelp' do
-        response = VCR.use_cassette 'taco' do
-           tacos.sorted_yelp_response
-         end
-        close_taco = VCR.use_cassette 'taco' do
-            tacos.closest
-          end
-
-        expect(close_taco).to eq response.first
-      end
-
-      # pending until swap to Google Places API
-      xit 'should return an apology if there are not tacos found via the search' do
-        response = VCR.use_cassette 'error' do
-            bad_request.sorted_yelp_response
-          end
-        expect(response).to eq "Sorry, I can't find any tacos for you."
       end
     end
   end
