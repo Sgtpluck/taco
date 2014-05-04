081dd1dbe6400050e90c6750aa39e4bd4194dd1d
Davida Marion <davidamarion@gmail.com>
2014-05-04 13:35:57 -0700

    makes replace global

--- app/assets/javascripts/other_location.js
+++ app/assets/javascripts/other_location.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('input.other_search').click(function (e) {
     e.preventDefault();
-    var url = $('#emergency').val().replace(/\s/,"+");
+    var url = $('#emergency').val().replace(/\s/g,"+");
     var err = $('.error');
     var closest = $('.closest');
     closest.empty();
