542be8821ace074dcb21c04fa4fb7b4994594be7
Davida Marion <davidamarion@gmail.com>
2014-05-22 12:59:20 -0700

    adds bundler.setup in application.rb for chef

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+Bundler.setup
 Bundler.require(:default, Rails.env)
 
 module TacoEmergency
