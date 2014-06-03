f03b53863b2eb409ec9f9e21d2ccaa0c97acc97e
Davida Marion <davidamarion@gmail.com>
2014-06-02 22:06:31 -0700

    adds pause to mimic ajax call

--- app/assets/javascripts/developer.js
+++ app/assets/javascripts/developer.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
   $('.swift').click(function (e) {
     e.preventDefault();
-    var closest = $('.closest').delay(5000);
+    var closest = $('.closest');
     closest.empty();
+    sleep(2000);
     closest.append("<br>I'm sorry, there are no experienced Swift Developers near you.");
+
+    function sleep(ms) {
+      var dt = new Date();
+      dt.setTime(dt.getTime() + ms);
+      while (new Date().getTime() < dt.getTime());
+      }
 
     });
 });
